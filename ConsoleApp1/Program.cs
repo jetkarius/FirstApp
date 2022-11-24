7228c7e8a2d3209f60a58453b170a6230f17776e
Jetkarius <boroksid@yandex.ru>
2022-11-24 21:06:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Yevgeny";
+            Console.WriteLine(MyName);
+        }
+    }
+}
